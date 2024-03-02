29faeb1337047dac6fb0d7b83d147e0b629dee30
Plawang Shishu <plawangshishu5029@gmail.com>
2024-03-02 17:13:06 +0530

    maxHeap

---
+++ maxHeap.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class maxHeap
+{
+public:
+    int arr[100];
+    int size;
+
+    maxHeap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    maxHeap h;
+    h.insert(45);
+    h.insert(56);
+    h.insert(90);
+    h.print();
+
+    return 0;
+}
