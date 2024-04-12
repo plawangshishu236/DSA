0162506d01cfda0f5134b2f84dc543e93100a7e1
Plawang Shishu <plawangshishu5029@gmail.com>
2024-04-13 01:27:36 +0530

    hashstringStriver

--- hash2Striver.cpp
+++ hash2Striver.cpp
@@ -7,9 +7,9 @@ int main()
     cin>>s;
 
     //pre compute
-    int hash[26] = {0};
+    int hash[256] = {0};
     for(int i =0;i<s.size();i++){
-        hash[s[i]- 'a'] ++;
+        hash[s[i]] ++;
     }
 
     int q;
@@ -18,6 +18,6 @@ int main()
         char c;
         cin>>c;
         //fetch
-        cout<<hash[c-'a']<<endl;
+        cout<<hash[c]<<endl;
     }
 }
