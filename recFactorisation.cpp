69869ab734f13ebe43c3b54ddaf86dbb5719f06d
Plawang Shishu <plawangshishu5029@gmail.com>
2024-04-12 07:37:26 +0530

    fact3 striver

--- recFactorisation.cpp
+++ recFactorisation.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 int fact(int a)
 {
-    int f = 1;
-    while (a > 0)
-    {
-        f = f * a;
-        a--;
-    }
-    return f;
-
+    // int f = 1;
     // while (a > 0)
     // {
-    //     return (a + sum(a - 1));
+    //     f = f * a;
+    //     a--;
     // }
+    // return f;
+
+    while (a >= 0)
+    {    if(a==0){return 1;}
+    else
+        {return (a * fact(a - 1));}
+    }
 
     // if (a==0){
     //     return 1;
